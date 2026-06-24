fe76ea88312053dcc699fb931489562e68af9ec3
ica95 <ichanr1810@gmail.com>
2026-06-24 22:46:40 +0800

    add edmin siderr

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,24 +3,13 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use App\Models\Dokter;
-use App\Models\Pasien;
-use App\Models\Layanan;
-use App\Models\JadwalDokter;
-use App\Models\Reservasi;
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        Dokter::factory(4)->create();
-
-        Pasien::factory(5)->create();
-
-        Layanan::factory(5)->create();
-
-        JadwalDokter::factory(10)->create();
-
-        Reservasi::factory(5)->create();
+        $this->call([
+            AdminSeeder::class,
+        ]);
     }
 }
